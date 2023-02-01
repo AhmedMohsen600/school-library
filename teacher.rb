b7bdb6446371ed5b59e132f13039ef3d8c7c90bc
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2023-02-01 16:28:52 +0200

    add class student, person and teacher

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require './person'
+
+
+class Teacher < Person
+    def initialize(specialization,name, age)
+     super(name, age)
+     @specialization = specialization
+    end
+
+    def can_use_services?
+        true
+    end
+end
+    
